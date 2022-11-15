c549f317376d1ed23e5bcf381841c22ff2d79970
Ryan Vargas <ryanr99@live.com>
2022-11-15 18:37:40 -0500

    Convert teaspoons function

--- conversion.py
+++ conversion.py
@@ -1,5 +1,20 @@
 #github.com/ryanrvargas
 
+def getTsp(unit, num):
+    match unit.lower():
+        case "ounces" | "oz":s
+            return num * 6
+        case "tbsp":
+            return num * 3
+        case "cup" | "cups":
+            return num * 48.6922
+        case "pint" | "pints":
+            return num * 96
+        case "quart" | "quarts":
+            return num * 192
+        case "gallon" | "gallons":
+            return num * 768
+            
 def getOzs(unit, num):
     match unit.lower():
         case "tsp":s
@@ -14,3 +29,5 @@ def getOzs(unit, num):
             return num * 32
         case "gallon" | "gallons":
             return num * 123
+            
+
