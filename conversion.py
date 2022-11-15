b69d424ae918303414a6d5f617386ef8341a1085
Ryan Vargas <ryanr99@live.com>
2022-11-15 17:53:09 -0500

    Convert ounces function

--- conversion.py
+++ conversion.py
@@ -1,1 +1,16 @@
-github.com/ryanrvargas
+#github.com/ryanrvargas
+
+def getOzs(unit, num):
+    match unit.lower():
+        case "tsp":s
+            return num * .16
+        case "tbsp":
+            return num * .5
+        case "cup" | "cups":
+            return num * 8
+        case "pint" | "pints":
+            return num * 16
+        case "quart" | "quarts":
+            return num * 32
+        case "gallon" | "gallons":
+            return num * 123
